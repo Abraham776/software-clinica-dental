26846d0a0379d3e845c8ba35615fdf4856df8013
Abraham <derokutojosi@gmail.com>
2022-03-11 13:19:04 -0700

    test input

--- src/pages/home/home-pages/HistoriaClinica.js
+++ src/pages/home/home-pages/HistoriaClinica.js
@@ -1,38 +1,47 @@
+// Sánchez Plazola José Abraham
+// 11/03/2022 Añadidos inputs
+
 import React from "react";
+import { Button, Col, Form, FormGroup, Input, Label } from "reactstrap";
 import Sidebar from "../componentes/sidebar";
 import "../home.scss";
 
 const HistoriaClinica = () => {
-
-
-
 	return (
-
-
-
 		<div className="home-contenido">
+			<Sidebar />
 
+			<div className="form-inputs">
+				{/* Inputs */}
+				<Form inline>
+					<FormGroup row>
+						<Col sm={3}>
+							<Label for="emailField">EMAIL:</Label>
+							<Input type="email" name="email" id="emailField"
+								placeholder="Enter your email" />
+						</Col>
 
+						<Col sm={3}>
+							<Label for="passwordField">PASSWORD:</Label>
+							<Input type="password" name="password"
+								id="passwordField"
+								placeholder="Enter your password" />
+						</Col>
+						
+						<Col sm={3}>
+							<Label for="passwordField">PASSWORD:</Label>
+							<Input type="password" name="password"
+								id="passwordField"
+								placeholder="Enter your password" />
+						</Col>
+					</FormGroup>
 
-
-
-			<div className="contenido-in">
-				<Sidebar />
-
-				Historial
-
+					<Button>Submit</Button>
+				</Form>
 			</div>
 
-
-
 		</div>
-
-
-
-
 	)
-
-
 };
 
 export default HistoriaClinica;
