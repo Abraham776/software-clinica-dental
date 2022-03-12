91b46e6e0a945818277aff6e9aeaae0f49ffdfc5
eRick0191 <100441641+eRick0191@users.noreply.github.com>
2022-03-11 22:47:23 -0700

    Update Opciones.js

--- src/pages/home/componentes/Opciones.js
+++ src/pages/home/componentes/Opciones.js
@@ -13,22 +13,22 @@ const Opciones = () => {
 
 	return (
 
-		<div className="Menu">
-			<Dropdown isOpen={dropdown} toggle={abrir} direction="right" >
+		 <div className="Menu">
+		    <Dropdown isOpen={dropdown} toggle={abrir} direction="right" >
 
-				<DropdownToggle caret>
-					<AiOutlineAudit className="me-2" />Historiales
-				</DropdownToggle>
-				<DropdownMenu className="menudemenu">
-					<DropdownItem header>Historiales</DropdownItem>
-					<DropdownItem className="itemsM"><NavLink to="/PacienteHistoHistoriales1" exact className="py-2 w-100 d-inline-block px-2">Historial 1</NavLink></DropdownItem>
-					<DropdownItem className="itemsM"><NavLink to="/PacienteHistoHistoriales2" exact className="py-2 w-100 d-inline-block px-2">Historial 2</NavLink></DropdownItem>
-					<DropdownItem className="itemsM"><NavLink to="/PacienteHistoHistoriales3" exact className="py-2 w-100 d-inline-block px-2">Historial 3</NavLink></DropdownItem>
-					<DropdownItem className="itemsM"><NavLink to="/PacienteHistoHistoriales4" exact className="py-2 w-100 d-inline-block px-2">Historial 4</NavLink></DropdownItem>
-					<DropdownItem className="itemsM"><NavLink to="/PacienteHistoHistoriales5" exact className="py-2 w-100 d-inline-block px-2">Historial 5</NavLink></DropdownItem>
+			<DropdownToggle caret>
+			<AiOutlineAudit className="me-2"/>Historiales 
+			</DropdownToggle>
+			<DropdownMenu className="menudemenu">
+			    <DropdownItem header>Historiales</DropdownItem>
+			    <DropdownItem className="itemsM"><NavLink   to="/PacienteHistoHistoriales1" exact className="py-2 w-100 d-inline-block px-2">Historial Clínico Completo</NavLink></DropdownItem>
+			    <DropdownItem className="itemsM"><NavLink   to="/PacienteHistoHistoriales2" exact className="py-2 w-100 d-inline-block px-2">Historial Clínico Ortodoncia</NavLink></DropdownItem>
+			    <DropdownItem className="itemsM"><NavLink   to="/PacienteHistoHistoriales3" exact className="py-2 w-100 d-inline-block px-2">Historial Clínico Odontopediatría</NavLink></DropdownItem>
+			    <DropdownItem className="itemsM"><NavLink   to="/PacienteHistoHistoriales4" exact className="py-2 w-100 d-inline-block px-2">Historial Clínico Endodoncia</NavLink></DropdownItem>
+			    <DropdownItem className="itemsM"><NavLink   to="/PacienteHistoHistoriales5" exact className="py-2 w-100 d-inline-block px-2">Historial Clínico General</NavLink></DropdownItem>
 
-				</DropdownMenu>
-			</Dropdown>
+			</DropdownMenu>
+		    </Dropdown>
 
 
 
